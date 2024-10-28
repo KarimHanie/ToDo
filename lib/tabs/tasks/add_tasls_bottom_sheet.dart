87f091b24eee3838feab41ad179061b7aa949303
Kareem_Hanie <karemabas29@gmail.com>
2024-10-28 20:24:38 +0300

    tasks model and tasks validation of task

--- lib/tabs/tasks/add_tasls_bottom_sheet.dart
+++ lib/tabs/tasks/add_tasls_bottom_sheet.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
+import 'package:to_do_app/widgets/custom_elevated_button.dart';
 import 'package:to_do_app/widgets/default_text_from_field/custom_text_form_field.dart';
+
+import '../../models/task_models.dart';
 
 class AddTask extends StatefulWidget {
   @override
@@ -8,22 +12,87 @@ class AddTask extends StatefulWidget {
 }
 
 class _AddTaskState extends State<AddTask> {
-  TextEditingController titleController=TextEditingController();
-  TextEditingController descriptionController=TextEditingController();
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  DateFormat dateFormat = DateFormat('dd/MM/yyyy');
+  DateTime selectedDate = DateTime.now();
+  var formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
+    TextStyle? titleMeduimStyle = Theme.of(context).textTheme.titleMedium;
     // TODO: implement build
-    return Container(
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            CustomTextFormField(controller: titleController, hintText: 'Enter Task title'),
-            CustomTextFormField(controller: descriptionController, hintText: 'Enter description'),
-
-          ],
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Container(
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              Text(
+                "add new task",
+                style: titleMeduimStyle,
+              ),
+              CustomTextFormField(
+                controller: titleController,
+                hintText: 'Enter Task title',
+                validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return 'title cannont be empty';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16),
+              CustomTextFormField(
+                controller: descriptionController,
+                hintText: 'Enter description',
+                validator: (value) {
+                  value?.trim();
+                  if (value == null || value.trim().isEmpty) {
+                    return 'description cannot tbe empty';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16),
+              Text('select Date',
+                  style:
+                      titleMeduimStyle?.copyWith(fontWeight: FontWeight.w400)),
+              SizedBox(height: 9),
+              InkWell(
+                  onTap: () async {
+                    DateTime? dateTime = await showDatePicker(
+                      initialEntryMode: DatePickerEntryMode.calendarOnly,
+                      context: context,
+                      initialDate: selectedDate,
+                      firstDate: DateTime.now(),
+                      lastDate: DateTime.now().add(Duration(days: 365)),
+                    );
+                    if (dateTime != null) {
+                      selectedDate = dateTime;
+                      setState(() {});
+                    }
+                  },
+                  child: Text('${dateFormat.format(selectedDate)}')),
+              SizedBox(height: 25),
+              CustomElevatedButton(
+                  label: 'Add',
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) addTask();
+                  }),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  void addTask() {
+    TaskModel task = TaskModel(
+      title: titleController.text,
+      description: descriptionController.text,
+      date: selectedDate,
     );
   }
 }
